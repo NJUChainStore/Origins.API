b3017b907f37bd459f18d487ca23eb5675871ae6
Chen Junda <smallda@outlook.com>
2018-05-17 22:33:30 +0800

    add encrytion algorithm

--- Origins.API/Data/ChainStore/DataEncryptor.cs
+++ Origins.API/Data/ChainStore/DataEncryptor.cs
@@ -1,7 +1,10 @@
 ﻿using Origins.API.Configs;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Origins.API.Data.ChainStore
@@ -15,18 +18,53 @@ namespace Origins.API.Data.ChainStore
             this.config = config;
         }
 
-        public string Encrypt(string info)
+        // https://stackoverflow.com/questions/202011/encrypt-and-decrypt-a-string-in-c
+        // a simple & dirty aes encryption algorithm
+        public string Encrypt(string clearText)
         {
-            byte[] bytes = System.Text.Encoding.Default.GetBytes(info);
-            var base64 = Convert.ToBase64String(bytes);
-            return base64;
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                byte[] IV = new byte[15];
+                new Random().NextBytes(IV);
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(config.Key, IV);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(IV) + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
         }
 
-        public string Decrypt(string encrypted)
+        public string Decrypt(string cipherText)
         {
 
-            byte[] bytes = Convert.FromBase64String(encrypted);
-            return System.Text.Encoding.Default.GetString(bytes);
+            byte[] IV = Convert.FromBase64String(cipherText.Substring(0, 20));
+            cipherText = cipherText.Substring(20).Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(config.Key, IV);
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
         }
 
 
